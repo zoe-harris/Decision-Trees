acce1d19acb68a01949c4df8ebb220b05f02c77d
Your Name <you@example.com>
2019-11-18 13:46:36 -0900

    Added method stubs to ApplyRules

--- Decision-Trees/src/ApplyRules.java
+++ Decision-Trees/src/ApplyRules.java
@@ -7,23 +7,37 @@ public class ApplyRules
 
     public class Representative
     {
+        // decision tree attributes
         boolean physicianFeeFreeze = true;
         boolean adoptBudgetResolution = true;
         boolean antiSatelliteTestBan = true;
-        String party;
 
-        public Representative(String phys, String budgetRes, String antiSat)
+        String party; // party as noted in text file
+        String classified; // party as classified by decision tree rules
+
+        public Representative(String phys, String budgetRes, String antiSat, String party)
         {
             if (phys.equals("n")) physicianFeeFreeze = false;
             if (budgetRes.equals("n")) adoptBudgetResolution = false;
             if (antiSat.equals("n")) antiSatelliteTestBan = false;
+            this.party = party;
         }
 
     }
 
     public static void populateRepresentatives(String file)
     {
+        // generate list of representative nodes
+    }
 
+    public static void classify()
+    {
+        // classify representatives as "democrat" or "republican"
+    }
+
+    public static void percentageIncorrect()
+    {
+        // display percentage of representatives incorrectly classified by tree rules
     }
 
 }
