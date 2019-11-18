301a890cb59c6c016421bbe506d6706e40fffe62
Your Name <you@example.com>
2019-11-18 12:04:15 -0900

    Commented in assignment instructions.

--- Decision-Trees/src/Main.java
+++ Decision-Trees/src/Main.java
@@ -1,27 +1,29 @@
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) /*throws Exception*/  {
+    public static void main(String[] args)
+    {
 
-        //  int num = args.length;
-        //	if (num != 1) {
-        //  	System.out.println("You need to specify the name of the datafile at the command line " );
-        //		return;
-        //	}
+        // 1. Run ID3.java on the TRAINING SET of examples to build a decision tree.
 
         ID3 me = new ID3();
-
-        long startTime = System.currentTimeMillis();	//  To print the time taken to process the data
 
         int status = me.readData("C:\\Users\\zharr\\Desktop\\train-house-votes-1984.txt");
         if (status <= 0) return;
 
         me.createDecisionTree();
 
-        long endTime = System.currentTimeMillis();
-        long totalTime = (endTime-startTime)/1000;
+        // 2. Save an electronic copy of the induced tree.
 
-        System.out.println( totalTime + " Seconds");
+        // 3. Next, write a Java program that implements the rules of the decision tree produced by ID3.
 
+        // 4. Next, apply your program to the TRAINING SET to make sure it correctly categorizes
+        //      each representative in the set as Democrat or Republican.
+
+        // 5a. Run program on TESTING SET.  For each incorrectly classified example, display
+        //      entire voting record and the affiliated party.
+
+        // 5b. After processing test data, display percentage of test set that was incorrectly classified.
 
     }
 
